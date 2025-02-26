ff22c49276176ddadb007c6ebb6bba1fe0b3aed1
Shrinivas Biradar <shrinivas@codebuddy.co>
2025-02-26 16:48:23 +0530

    prettier: rules update and addig ignore file

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,11 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  jsxSingleQuote: false,
+  endOfLine: 'lf',
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: 'preserve',
 };
