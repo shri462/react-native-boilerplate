a93737dddf641d4e3d2122ce377b157e378b7539
Shrinivas Biradar <shrinivasbiradar462@gmail.com>
2025-05-22 01:38:25 +0530

    :sparkles: feat(root): adding reactotron to debug api calls

--- App.tsx
+++ App.tsx
@@ -3,12 +3,20 @@ import RootNavigator from "./src/navigation/RootNavigators";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import toastConfig from "./src/utils/toastConfig";
 import Toast from "react-native-toast-message";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./src/query-client/queryClient";
+
+if (process.env.NODE_ENV === "development") {
+  require("./src/query-client/reactotronConfig");
+}
 
 function App(): React.JSX.Element {
   return (
     <GestureHandlerRootView>
-      <RootNavigator />
-      <Toast config={toastConfig} position="bottom" bottomOffset={60} />
+      <QueryClientProvider client={queryClient}>
+        <RootNavigator />
+        <Toast config={toastConfig} position="bottom" bottomOffset={60} />
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 }
---
+++ src/query-client/reactotronConfig.ts
@@ -0,0 +1,20 @@
+import Reactotron from "reactotron-react-native";
+import {
+  QueryClientManager,
+  reactotronReactQuery,
+} from "reactotron-react-query";
+import { queryClient } from "./queryClient";
+
+const queryClientManager = new QueryClientManager({
+  queryClient,
+});
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+Reactotron.use(reactotronReactQuery(queryClientManager))
+  .configure({
+    onDisconnect: () => {
+      queryClientManager.unsubscribe();
+    },
+  })
+  .useReactNative()
+  .connect();
